b0b6948bd49fb8695104a05adac8ca8e15ef4811
jxxcr <jixxcr@qq.com>
2025-05-07 21:13:49 +0800

    add orth option for build cut

--- src/mdkits/build_cli/cut_surface.py
+++ src/mdkits/build_cli/cut_surface.py
@@ -4,6 +4,7 @@ from ase import build
 import click, os
 from mdkits.util import arg_type, out_err
 from mdkits.build_cli import supercell
+import numpy as np
 
 
 @click.command(name='cut')
@@ -12,16 +13,27 @@ from mdkits.build_cli import supercell
 @click.option('--size', type=click.Tuple([int, int, int]), help='surface size')
 @click.option('--vacuum', type=float, help='designate vacuum of surface, default is None', default=0.0, show_default=True)
 @click.option('--cell', type=arg_type.Cell, help='set xyz file cell, --cell x,y,z,a,b,c')
-def main(atoms, face, vacuum, size, cell):
+@click.option('--orth', is_flag=True, help='orthogonalize cell')
+def main(atoms, face, vacuum, size, cell, orth):
     """cut surface"""
     out_err.check_cell(atoms, cell)
 
     surface = build.surface(atoms, face, size[2], vacuum=vacuum/2)
     super_surface = supercell.supercell(surface, size[0], size[1], 1)
 
+    if orth:
+        super_surface_cell = super_surface.cell.cellpar()
+        gamma = super_surface_cell[-1]
+        if gamma != 90:
+            b = np.sin(np.radians(gamma)) * super_surface_cell[1]
+            super_surface_cell[1] = b
+            super_surface_cell[-1] = 90
+            super_surface.set_cell(super_surface_cell)
+
     o = f"{atoms.filename.split('.')[-2]}_{face[0]}{face[1]}{face[2]}_{size[0]}{size[1]}{size[2]}.cif"
     super_surface.write(o)
-    print(os.path.abspath(o))
+    out_err.cell_output(super_surface.cell.cellpar())
+    out_err.path_output(o)
 
 
 if __name__ == '__main__':
