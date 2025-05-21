60baeff00fa34fa6bda773eb45e3ac81b9450f23
jxxcr <jixxcr@qq.com>
2025-05-21 17:18:47 +0800

    fix build solution bug and publish

--- src/mdkits/build_cli/build_solution.py
+++ src/mdkits/build_cli/build_solution.py
@@ -2,29 +2,75 @@ import click, os
 from mdkits.util import arg_type
 from importlib import resources
 from mdkits.cli import convert
-import tempfile
+import tempfile, platform, subprocess
 
 
 @click.command(name="solution")
 @click.argument("filename", type=click.Path(exists=True), nargs=-1)
 @click.option('--infile', is_flag=True, help="read input mode")
-@click.option('--install', is_flag=True, help="install julia and packmol")
+@click.option('--install_julia', is_flag=True, help="install julia for system")
+@click.option('--install_packmol', is_flag=True, help="init julia install packmol")
 @click.option('--water_number', type=int, help="number of water molecules", default=0, show_default=True)
 @click.option('-n', type=int, multiple=True, help="number of molecules")
 @click.option('--tolerance', type=float, help="tolerance of solution", default=3.5, show_default=True)
 @click.option('--cell', type=arg_type.Cell, help="set cell, a list of lattice: --cell x,y,z or x,y,z,a,b,c")
 @click.option('--gap', type=float, help="gap between solution and cell", default=1, show_default=True)
-def main(filename, infile, install, water_number, n, tolerance, cell, gap):
+def main(filename, infile, install_julia, install_packmol, water_number, n, tolerance, cell, gap):
     """
     build solution model
     """
-    if install:
+    os_name = platform.system()
+
+    if install_julia:
+        if os_name in ("Darwin", "Linux", "FreeBSD"):
+            try:
+                subprocess.run(
+                ["curl", "-fsSL", "https://install.julialang.org", "|", "sh"],
+                shell=True,  
+                check=True,
+            )
+                print("Julia installed (macOS, Linux, FreeBSD).")
+            except subprocess.CalledProcessError as e:
+                print(f"Julia install failed: {e}")
+        elif os_name == "Windows":
+            try:
+                subprocess.run(
+                    [
+                        "winget",
+                        "install",
+                        "--name",
+                        "Julia",
+                        "--id",
+                        "9NJNWW8PVKMN",
+                        "-e",
+                        "-s",
+                        "msstore",
+                    ],
+                    check=True,
+                )
+                print("Julia installed (Windows).")
+            except subprocess.CalledProcessError as e:
+                print(f"Julia install failed: {e}")
+        else:
+            print(f"unsupport os: {os_name}")
+
+    if install_packmol:
         import julia
         julia.install()
+
+        from julia.api import Julia
+        jl = Julia(compiled_modules=False)
+
+        from julia import Pkg, Main
+        Pkg.add("PyCall")
         Pkg.activate("Packmol", shared=True)
         Pkg.add("Packmol")
         Main.exit()
     else:
+        if os_name in ("Darwin", "Linux"):
+            from julia.api import Julia
+            jl = Julia(compiled_modules=False)
+
         from julia import Pkg, Main
 
         if cell is None:
@@ -32,6 +78,9 @@ def main(filename, infile, install, water_number, n, tolerance, cell, gap):
 
         if len(filename) == 0 and water_number == 0:
             raise ValueError("at least one file should be provided, or water_number should be greater than 0")
+
+        if os_name in ("Darwin", "Linux"):
+            Pkg.activate("Packmol", shared=True)
 
         while True:
             try:
