1ba5266bd7e0375a57cb214703bf485b0d717d38
jxxcr <jixxcr@qq.com>
2025-05-21 14:22:49 +0800

    mix import julia bug

--- src/mdkits/build_cli/build_solution.py
+++ src/mdkits/build_cli/build_solution.py
@@ -27,63 +27,63 @@ def main(filename, infile, install, water_number, n, tolerance, cell, gap):
     else:
         from julia import Pkg, Main
 
-    if cell is None:
-        raise ValueError("cell should be provided")
+        if cell is None:
+            raise ValueError("cell should be provided")
 
-    if len(filename) == 0 and water_number == 0:
-        raise ValueError("at least one file should be provided, or water_number should be greater than 0")
+        if len(filename) == 0 and water_number == 0:
+            raise ValueError("at least one file should be provided, or water_number should be greater than 0")
 
-    while True:
-        try:
-            Main.using("Packmol")
-            break
-        except Exception:
-            pass
+        while True:
+            try:
+                Main.using("Packmol")
+                break
+            except Exception:
+                pass
 
-    if infile:
-        for file in filename:
-            Main.run_packmol(file)
-    else:
-        if len(n) != len(filename):
-            raise ValueError("number of -n should be equal to number of files")
+        if infile:
+            for file in filename:
+                Main.run_packmol(file)
+        else:
+            if len(n) != len(filename):
+                raise ValueError("number of -n should be equal to number of files")
 
-        temp_file = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
-        backslash = "\\"
+            temp_file = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
+            backslash = "\\"
 
-        structure_input = {}
-        output_filenames = []
-        if len(filename) > 0:
-            for index, file in enumerate(filename):
-                structure_input[file] = f"structure {os.path.join(os.getcwd(), file.replace(backslash, '/').replace('./', ''))}\n  number {n[index]}\n  inside box {gap} {gap} {gap} {cell[0]-gap} {cell[1]-gap} {cell[2]-gap}\nend structure\n"
+            structure_input = {}
+            output_filenames = []
+            if len(filename) > 0:
+                for index, file in enumerate(filename):
+                    structure_input[file] = f"structure {os.path.join(os.getcwd(), file.replace(backslash, '/').replace('./', ''))}\n  number {n[index]}\n  inside box {gap} {gap} {gap} {cell[0]-gap} {cell[1]-gap} {cell[2]-gap}\nend structure\n"
 
-                output_filenames.append(f"{file.replace(backslash, '/').split('.')[-2].split('/')[-1]}_{n[index]}")
+                    output_filenames.append(f"{file.replace(backslash, '/').split('.')[-2].split('/')[-1]}_{n[index]}")
 
-        if water_number > 0:
-            water_path = resources.files('mdkits.build_cli').joinpath('water.xyz')
+            if water_number > 0:
+                water_path = resources.files('mdkits.build_cli').joinpath('water.xyz')
 
-            structure_input["water"] = f"structure {water_path}\n  number {water_number}\n  inside box {gap} {gap} {gap} {cell[0]-gap} {cell[1]-gap} {cell[2]-gap}\nend structure\n"
+                structure_input["water"] = f"structure {water_path}\n  number {water_number}\n  inside box {gap} {gap} {gap} {cell[0]-gap} {cell[1]-gap} {cell[2]-gap}\nend structure\n"
 
-            output_filenames.append(f"{str(water_path).replace(backslash, '/').split('.')[-2].split('/')[-1]}_{water_number}")
+                output_filenames.append(f"{str(water_path).replace(backslash, '/').split('.')[-2].split('/')[-1]}_{water_number}")
 
-        output_filename = "-".join(output_filenames) + ".xyz"
-        head_input = f"tolerance {tolerance}\nfiletype xyz\noutput {os.path.join(os.getcwd(), output_filename)}\npbc {cell[0]} {cell[1]} {cell[2]}\n"
+            output_filename = "-".join(output_filenames) + ".xyz"
+            head_input = f"tolerance {tolerance}\nfiletype xyz\noutput {os.path.join(os.getcwd(), output_filename)}\npbc {cell[0]} {cell[1]} {cell[2]}\n"
 
-        total_input = head_input + "\n".join(structure_input.values())
+            total_input = head_input + "\n".join(structure_input.values())
     
-        temp_file.write(total_input)
-        temp_file.flush()
-        temp_file.close()
+            temp_file.write(total_input)
+            temp_file.flush()
+            temp_file.close()
 
-        Main.run_packmol(temp_file.name)
+            Main.run_packmol(temp_file.name)
 
-        if os.path.exists(temp_file.name):
-            os.remove(temp_file.name)
+            if os.path.exists(temp_file.name):
+                os.remove(temp_file.name)
 
-    print("="*15)
-    print(total_input)
-    print("="*15)
-    convert.main([output_filename, "-c", "--cell", ",".join([str(a) for a in cell])], standalone_mode=False)
-    Main.exit()
+        print("="*15)
+        print(total_input)
+        print("="*15)
+        convert.main([output_filename, "-c", "--cell", ",".join([str(a) for a in cell])], standalone_mode=False)
+        Main.exit()
 
 
 if __name__ == "__main__":
