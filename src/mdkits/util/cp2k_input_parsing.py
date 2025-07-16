c8e8bc80e1d8a26816f0baf7e6af804939f85e7b
jxxcr <jixxcr@qq.com>
2025-07-16 14:31:48 +0800

    fix prase cell bug

--- src/mdkits/util/cp2k_input_parsing.py
+++ src/mdkits/util/cp2k_input_parsing.py
@@ -30,7 +30,11 @@ def parse_cell():
                 if len(cell) == 3:
                     cell.extend([90.0, 90.0, 90.0])
 
-                out_err.cell_output(cell)
+                if len(cell) == 0:
+                    print("parse failed")
+                    return [0., 0., 0., 90., 90., 90.]
+                else:
+                    print(f"parsed cell: x = {cell[0]}, y = {cell[1]}, z = {cell[2]}, a = {cell[3]}\u00B0, b = {cell[4]}\u00B0, c = {cell[5]}\u00B0")
                 return cell
         except FileNotFoundError:
             return [0., 0., 0., 90., 90., 90.]
