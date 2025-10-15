42630358655407fbf6fa0a9f3cd0a76006b4b807
jxxcr <jixxcr@qq.com>
2025-10-15 11:19:07 +0800

    add -r to md msd

--- src/mdkits/md_cli/msd.py
+++ src/mdkits/md_cli/msd.py
@@ -2,17 +2,25 @@ import click
 import MDAnalysis as mda
 import MDAnalysis.analysis.msd as msd
 import numpy as np
+from mdkits.util import arg_type
 
 
 @click.command(name="msd")
 @click.argument("filename", type=click.Path(exists=True))
 @click.argument('type', type=click.Choice(['xyz', 'xy', 'yz', 'xz', 'x', 'y', 'z']))
 @click.argument("group", type=str)
-def main(filename, type, group):
+@click.option('-r', type=arg_type.FrameRange, help='range of frame to analysis')
+def main(filename, type, group, r):
     """analysis msd along the given axis"""
     u = mda.Universe(filename)
     MSD = msd.EinsteinMSD(u, select=group, msd_type=type, fft=True)
-    MSD.run(verbose=True)
+    if r is not None:
+        if len(r) == 2:
+            MSD.run(start=r[0], stop=r[1], verbose=True)
+        elif len(r) == 3:
+            MSD.run(start=r[0], stop=r[1], step=r[2], verbose=True)
+    else:
+        MSD.run(verbose=True)
 
     data = np.arange(1, MSD.n_frames + 1).reshape(-1, 1)
     s = "_"
