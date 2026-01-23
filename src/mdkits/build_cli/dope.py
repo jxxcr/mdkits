3959f64f53c7470576637781d6692eeb1f2a1a2d
jxxcr <jixxcr@qq.com>
2026-01-23 22:53:09 +0800

    add option to dope kit

--- src/mdkits/build_cli/dope.py
+++ src/mdkits/build_cli/dope.py
@@ -6,9 +6,10 @@ import numpy as np
 @click.command(name='dope')
 @click.argument('atoms', type=arg_type.Structure)
 @click.option('--group', type=str, required=True, help='group to dope')
-@click.option('--element', type=str, required=True, help='element to dope in only once')
+@click.option('--element', type=str, required=True, help='element to dope in some times')
+@click.option('--times', type=int, default=1, help='number of atoms to dope')
 @click.option('--seed', type=int, default=20000203, help='random seed')
-def main(atoms, group, element, seed):
+def main(atoms, group, element, times, seed):
     """Dope atoms in the structure"""
     np.random.seed(seed)
 
@@ -18,8 +19,9 @@ def main(atoms, group, element, seed):
     atomgroup = u.select_atoms(f"{group}")
     if atomgroup.n_atoms == 0:
         raise ValueError(f"No atoms found for group: {group}")
-    selected_index = np.random.choice(atomgroup.indices)
-    u.atoms[selected_index].element = element
+    selected_indices = np.random.choice(atomgroup.indices, size=times, replace=False)
+    for idx in selected_indices:
+        u.atoms[idx].element = element
 
     element, count = np.unique(u.select_atoms("all").elements, return_counts=True)
     o = f"{atoms.filename.split('.')[0]}_{'-'.join([f'{e}{c}' for e, c in zip(element, count)])}_doped.cif"
